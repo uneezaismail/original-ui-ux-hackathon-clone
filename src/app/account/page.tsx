3cb9d48a6697345338bff72ac03b2f1b4b662e66
uneezaismail <ismailrahet@gmail.com>
2025-02-01 15:01:24 +0500

    ui-ux-hackathon-clone without any changing

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,32 +1,3 @@
-// import Login from '@/components/Login'
-// import NavImage from '@/components/NavImage'
-// import Delivery from '@/components/ourDelivery'
-// import Register from '@/components/Register'
-// import React from 'react'
-
-// const Account = () => {
-//   return (
-//     <section className='w-full'>
-//         <NavImage heading={"My Account"} path={"Home"} currentPage={"My account"}/>
-//     <div className='w-fit bg-yellow-700 md:mx-auto flex flex-col md:flex-row gap-y-24 md:gap-y-20 py-16'>
-// <div>
-//     <Login/>
-// </div>
-// <div>
-//     <Register/>
-// </div>
-//     </div>
-// <div>
-//     <Delivery/>
-// </div>
-//     </section>
-//   )
-// }
-
-// export default Account
-
-
-
 
 import Login from '@/components/Login'
 import NavImage from '@/components/NavImage'
